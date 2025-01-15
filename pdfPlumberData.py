8a44dcb5f945c5232bc340a24884d5690ae16667
TK Mokoma <tkwwwtkzacom@gmail.com>
2025-01-15 19:49:20 +0200

    The cleanData function completion

--- pdfPlumberData.py
+++ pdfPlumberData.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 import pdfplumber
 import csv
 
@@ -6,10 +7,6 @@ def uploadPDF(PDF, csv_path):
     # Open the PDF file
     with pdfplumber.open(PDF) as pdf, open(csv_path, 'w', newline='', encoding="utf-8") as csv_file:
         csv_writer = csv.writer(csv_file)  # Use the opened file object here
-
-        #  # Create/open the CSV file for writing
-        #     with open(csv_path, "w", newline="", encoding="utf-8") as csv_file:
-        #         csv_writer = csv.writer(csv_file)
         
         # Iterate through each page
         for page_num, page in enumerate(pdf.pages):
@@ -37,12 +34,29 @@ def uploadPDF(PDF, csv_path):
 # Path to the PDF file
 pdf_file_path = 'DOC-20250101-WA0023..pdf'  # Corrected file path
 
+###################################################
+
+# A function to clean the CSV generated after uploading the csv
+def cleanData(csv_path):
+    # specifying the column names
+    columnNames =['Transaction Date','From','To','Transaction Amount','Balance','Description']
+
+    # Read the CSV file, handling errors and potentially skipping bad lines
+    TransactionsCSV = pd.read_csv(csv_path, on_bad_lines='warn',usecols=range(6),names=columnNames)
+
+    # Display the first 20 rows to inspect the data
+    print(TransactionsCSV.head())
+
+    # saving the cleaned data to Transactions_clean.csv
+    return TransactionsCSV.to_csv("Transactions_functionTestclean.csv", index=False)
+    
+#################################################################################################
 # Path to the output CSV file
 csv_file_path = 'transactions_functionTest.csv'
 
 work = uploadPDF(pdf_file_path,csv_file_path)
-print(work)
-#################################################################################################
+cleanData(work)
+
 # st.title("Hello, Streamlit!")
 # st.write("Welcome to your first Streamlit app.")
 # st.text_input("Enter your name:")
